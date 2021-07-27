db5bf9843bd07fd0e42a75f4d68b6cbceb85db56
sundar rao <sundar.badagala@gmail.com>
2021-07-27 22:55:13 +0530

    add cart length to cart button in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,31 +1,47 @@
-import React from 'react'
-import {Container, Row, Col, Button, Badge} from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
+import {Container, Row, Col, Button, Badge} from 'react-bootstrap'
+import {connect} from 'react-redux'
 
-function Navbar() {
+function Navbar({cart}) {
+    const [cartCount, setCartCount]= useState(0)
+    useEffect(()=>{
+        let count=0
+        cart && cart.forEach(item => {
+            count = count + item.qty
+        })
+        setCartCount(count)
+    },[cart])
     return (
-        <div className='bg-primary'>
-        <Container className='p-2'>
+        <Container fluid className='bg-primary p-2'>
             <Row>
-                <Col className=''>
-                    <Link to='/' className='h4 font-weight-bold font-italic text-dark text-decoration-none'>
-                        Shoping Cart
-                    </Link>
+                <Col className='ml-3'>
+                    <h3>
+                        <Link  to='/' className='text-light text-decoration-none font-weight-bold font-italic'>
+                            Footwear
+                        </Link>
+                    </h3>
                 </Col>
-                <Col className='text-right'>
-                    <Link to='/cart'>
-                    <Button variant='warning'>
-                        Cart
-                        <Badge pill variant='danger' className='ml-2'>
-                            2
-                        </Badge>
+                <Col className='d-flex justify-content-end mr-3'>
+                <Link to='/cart'>
+                    <Button variant='warning'  className='font-weight-bold'>
+                        <i className="fas fa-shopping-cart"></i>
+                        <span className='m-2'>My Cart </span>
+                        {
+                            cartCount ?<Badge variant='danger'>{cartCount}</Badge> : null
+                        }
                     </Button>
-                    </Link>
+                </Link>
                 </Col>
             </Row>
         </Container>
-        </div>
     )
 }
 
-export default Navbar
+const mapStateToProps=(state)=>{
+    return{
+        cart : state.shop.cart
+    }
+}
+
+export default connect(mapStateToProps, null)(Navbar)
