059e40de9c6fbe06923ba04d5ff77f97b712bc94
sundar rao <sundar.badagala@gmail.com>
2021-07-27 20:57:41 +0530

    create navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {Container, Row, Col, Button, Badge} from 'react-bootstrap'
 
 function Navbar() {
     return (
-        <div>
-            thi sis nav fiel
+        <div className='bg-primary'>
+        <Container className='p-2'>
+            <Row>
+                <Col className='h4 font-weight-bold font-italic'>
+                    Shoping Cart
+                </Col>
+                <Col className='text-right'>
+                    <Button variant='warning'>
+                        Cart
+                        <Badge pill variant='danger' className='ml-2'>
+                            2
+                        </Badge>
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
         </div>
     )
 }
