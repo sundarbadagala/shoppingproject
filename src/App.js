dd153232d0b1e4ec9151f694cec58cb7e140fdcb
sundar rao <sundar.badagala@gmail.com>
2021-07-27 21:08:34 +0530

    apply routing to navbar

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import Navbar from './components/Navbar/Navbar';
 import Cart from './components/Cart/Cart';
 import {Provider} from 'react-redux'
 import store from './redux/store'
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <Provider store={store}>
+    <Router>
       <Navbar/>
-      <Products/>
-      <Cart/>
+      <Switch>
+      <Route exact path='/'><Products/></Route>
+      <Route path='/cart'><Cart/></Route>
+      </Switch>
+      </Router>
     </Provider>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import {Container, Row, Col, Button, Badge} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
     return (
         <div className='bg-primary'>
         <Container className='p-2'>
             <Row>
-                <Col className='h4 font-weight-bold font-italic'>
-                    Shoping Cart
+                <Col className=''>
+                    <Link to='/' className='h4 font-weight-bold font-italic text-dark text-decoration-none'>
+                        Shoping Cart
+                    </Link>
                 </Col>
                 <Col className='text-right'>
+                    <Link to='/cart'>
                     <Button variant='warning'>
                         Cart
                         <Badge pill variant='danger' className='ml-2'>
                             2
                         </Badge>
                     </Button>
+                    </Link>
                 </Col>
             </Row>
         </Container>
